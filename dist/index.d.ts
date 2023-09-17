f080f9420edda48f285c803172dd931cb31bd796
Sebastian Bayerl <sebisebs@gmail.com>
2023-09-17 19:44:23 +0200

    :green_heart: Fix publishing

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { PlayerCore, ICoreOptions, ISoundsQueueOptions, IPlayOptions } from './library/core';
-export { PlayerSound, ISoundAttributes, ISound } from './library/sound';
--- dist/library/request.d.ts
+++
@@ -1,9 +0,0 @@
-import { IOnProgress } from './sound';
-export interface IRequested {
-    url: string;
-    loadingProgress: number;
-    onLoading?: IOnProgress;
-}
-export declare class PlayerRequest {
-    getArrayBuffer(requested: IRequested): Promise<ArrayBuffer>;
-}
